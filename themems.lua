cd3bd206e4bd5bd5c1f2d182fad901a80d1837dd
pubertus <peedresm@gmail.com>
2025-02-17 14:14:40 -0600

    Update themems.lua

--- themems.lua
+++ themems.lua
@@ -2,13 +2,13 @@ local httpService = game:GetService('HttpService')
 local ThemeManager = {} do
 	ThemeManager.Folder = 'LinoriaLibSettings'
 	-- if not isfolder(ThemeManager.Folder) then makefolder(ThemeManager.Folder) end
-
+--httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"6759b3","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
 	ThemeManager.Library = nil
 	ThemeManager.BuiltInThemes = {
-		['Default'] 		= { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"6759b3","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
+		['AmoPurple'] 		= { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1a1a1a","AccentColor":"a136ff","BackgroundColor":"0f0f0f","OutlineColor":"262626"}') },
 		['Amoled'] = { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1a1a1a","AccentColor":"4d4d4d","BackgroundColor":"0f0f0f","OutlineColor":"262626"}') },
 		['BBot'] 			= { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1e1e","AccentColor":"7e48a3","BackgroundColor":"232323","OutlineColor":"141414"}') },
-		['AmoPurple'] = { 4, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1a1a1a","AccentColor":"a136ff","BackgroundColor":"0f0f0f","OutlineColor":"262626"}') },
+		['OldDefault'] = { 4, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"6759b3","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
 		['Fatality']		= { 5, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1842","AccentColor":"c50754","BackgroundColor":"191335","OutlineColor":"3c355d"}') },
 		['Jester'] 			= { 6, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"db4467","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
 		['Mint'] 			= { 7, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"3db488","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
